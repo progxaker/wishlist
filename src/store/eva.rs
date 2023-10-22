78d5206e046211133e3be9fca80edfe6e0fa15fa
Eduard Mielieshkin <proxaker@workmail.com>
2023-10-22 22:36:54 +0300

    added Eva support for various product volumes

--- src/store/eva.rs
+++ src/store/eva.rs
@@ -4,9 +4,22 @@ use crate::error::Error;
 use crate::store::ItemInfo;
 use crate::utils;
 
+use serde::Deserialize;
+use urlencoding::encode;
+
 pub struct Eva
 {
     name: &'static str,
+}
+
+#[derive(Deserialize)]
+struct SearchChild
+{
+    is_present: bool,
+    name: String,
+    #[serde(rename="externalAttr100050")]
+    attribute: i64,
+    price: f64,
 }
 
 impl Eva
@@ -18,35 +31,64 @@ impl Eva
 
     fn dataURL(&self, id: &str) -> String
     {
-        let (product_id, attribute_value) = id.split_once("-").unwrap();
-        format!("https://eva.ua/ua/{}/#/{}/", product_id, attribute_value)
+        let search_api_url = "https://pwa-api.eva.ua/api/catalog/eva_catalog_default/product/_search";
+        // TODO: is_precense
+        let source_include_value = "configurable_children.externalAttr100050,configurable_children.price,configurable_children.name,configurable_children.is_present";
+        let request_value_raw = format!("{{\"query\":{{\"bool\":{{\"filter\":{{\"terms\":{{\"id\":[{}]}}}}}}}}}}", id);
+        let request_value = encode(&request_value_raw);
+        format!("{}?_source_include={}&request={}", search_api_url, source_include_value, request_value)
+    }
+
+    fn productURL(&self, id: &str, attribute: &str) -> String
+    {
+        format!("https://eva.ua/ua/{}/#/{}/", id, attribute)
     }
 
     pub async fn get(&self, id: &str) -> Result<ItemInfo, Error>
     {
-        // The items in Epic store are very well-defined. The info is
-        // embeded in the HTML as Linked Data JSON.
-        let url = self.dataURL(id);
-        let content = utils::get(&url).await?;
-        let (s, _, _) = utils::findSubStr(
-            &content, "type=\"application/ld+json\"", "</script>").ok_or_else(
-            || rterr!("Invalid Eva item page"))?;
-        let begin = s.find("{").ok_or_else(
-            || rterr!("Failed to find beginning of JSON"))?;
-        let json_str = &s[begin..];
+        let (product_value, attribute_value) = id.split_once("-").ok_or_else(|| {
+            rterr!("Failed to parse ID")
+        })?;
 
-        let data: json::Value = serde_json::from_str(json_str).map_err(
-            |_| rterr!("Failed to parse JSON"))?;
-        let price = data["offers"]["price"].as_f64()
-            .ok_or_else(|| rterr!("Failed to extract price"))?;
-        let name = data["name"].as_str().ok_or_else(
-            || rterr!("Failed to extract name"))?;
+        let product_id = product_value.strip_prefix("pr").ok_or_else(|| {
+            rterr!("Failed to extract product ID")
+        })?;
+
+        let dataUrl = self.dataURL(product_id);
+
+        let json_str = utils::get(&dataUrl).await?;
+
+        let productUrl = self.productURL(product_value, attribute_value);
+
+        let data: json::Value = serde_json::from_str(&json_str).map_err(|err| {
+            rterr!("Failed to parse JSON: {}", err)
+        })?;
+
+        let configurable_children_json = &data["hits"]["hits"][0]["_source"]["configurable_children"];
+
+        let configurable_children: Vec<SearchChild> = serde_json::from_value(
+            configurable_children_json.clone()).map_err(|err| {
+                rterr!("Failed to parse configurable children: {}", err)
+            })?;
+
+        // FIXME: the price can be in JSON even if product isn't present
+        let search_result = configurable_children.iter().find(|child|
+            child.attribute == attribute_value.parse::<i64>().unwrap_or_default());
+
+        let (is_present, name, price) = match search_result {
+            Some(child) => (child.is_present, child.name.clone(), child.price),
+            None => (false, String::new(), 0.0),
+        };
+
+        if name.is_empty() || price == 0.0 {
+            return Err(rterr!("Failed to find product sub-type"));
+        }
 
         let mut item = ItemInfo::new(self.name, id);
         item.name = name.to_owned();
         item.price = (price * 100.0) as i64;
-        item.price_str = format!("₴{}", price);
-        item.url = url;
+        item.price_str = if is_present { format!("₴{}", price) } else { "Out of stock".to_string() };
+        item.url = productUrl;
         Ok(item)
     }
 }
